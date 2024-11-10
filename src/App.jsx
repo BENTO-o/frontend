e35d4fe92ca17d5dd3ae13bdba71eb291c8c7092
Sunbeom <wsbs9805@naver.com>
2024-11-10 17:59:32 +0900

    :sparkles: 라우트 분리
    - 라우트 분기 처리
    - 의존성 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./App.css";
+import Landing from "./domains/landing/Landing";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Login from "./domains/login/Login";
+import Signup from "./domains/signup/Signup";
 
-import './App.css'
-import Signup from './domains/signup/Signup'
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
-    <>
-      <Signup />
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
