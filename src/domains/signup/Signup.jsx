ef232bee8abafe84dbd4cd22c263bcd1a986979a
Sunbeom <wsbs9805@naver.com>
2024-11-13 03:07:43 +0900

    :sparkles: 회원가입 기능 구현
    - 회원가입 페이지 기능 구현
    - 회원가입 API 작성

--- src/domains/signup/Signup.jsx
+++ src/domains/signup/Signup.jsx
@@ -1,5 +1,5 @@
+import { useState } from "react";
 import {
-  ForgetPW,
   FormCheckbox,
   FormContainer,
   FormContentLeft,
@@ -13,10 +13,53 @@ import {
   Txt20,
   Txt24Bold,
   TxtBtn,
-  TxtCTA,
 } from "../../common/common";
+import { signup } from './services/index';
+import { useMutation } from "@tanstack/react-query";
 
 function Signup() {
+  const [formData, setFormData] = useState({
+    email: '',
+    username: '',
+    password: '',
+    passwordConfirm: '',
+    checkedRule: false,
+    checkedPersonalInfo: false,
+  });
+  
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'checkbox' ? checked : value
+    }));
+  };
+
+  const register = useMutation({
+    mutationFn: signup,
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log('에러 발생! 아래 메시지를 확인해주세요.', error);
+    },
+  });
+
+  const onClickSignupBtn = async () => {
+    if (formData.password !== formData.passwordConfirm) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    } else if (!formData.checkedRule || !formData.checkedPersonalInfo) {
+      alert('약관에 동의해주세요.');
+      return;
+    } else if (!formData.email || !formData.username || !formData.password) {
+      alert('모든 항목을 입력해주세요.');
+      return;
+    } else {
+      register.mutate({ email: formData.email, username: formData.username, password: formData.password });
+    }
+  };
+
   return (
     <PageLayout>
       <LogoTxt>BENTO</LogoTxt>
@@ -25,39 +68,38 @@ function Signup() {
         <FormLayout mt="20px" mb="20px">
           <FormContentVertical>
             <Txt20>이메일</Txt20>
-            <LoginFormInput type="text" />
+            <LoginFormInput type="text" name="email" value={formData.email} onChange={handleInputChange} />
           </FormContentVertical>
-          <FormContentVertical>
+          {/* <FormContentVertical>
             <Txt20>이름</Txt20>
-            <LoginFormInput type="text" />
-          </FormContentVertical>
+            <LoginFormInput type="text" name="username" value={formData.username} onChange={handleInputChange} />
+          </FormContentVertical> */}
           <FormContentVertical>
             <Txt20>닉네임</Txt20>
-            <LoginFormInput type="text" />
+            <LoginFormInput type="text" name="username" value={formData.username} onChange={handleInputChange} />
           </FormContentVertical>
           <FormContentVertical>
             <Txt20>비밀번호</Txt20>
-            <LoginFormInput type="password" />
+            <LoginFormInput type="password" name="password" value={formData.password} onChange={handleInputChange} />
           </FormContentVertical>
           <FormContentVertical>
             <Txt20>비밀번호 확인</Txt20>
-            <LoginFormInput type="password" />
+            <LoginFormInput type="password" name="passwordConfirm" value={formData.passwordConfirm} onChange={handleInputChange} />
           </FormContentVertical>
           <FormContentLeft>
-          <FormCheckbox type="checkbox" />
+          <FormCheckbox type="checkbox" name="checkedRule" value={formData.checkedRule} onChange={handleInputChange} />
           <Txt16>(필수)</Txt16>
           <TxtBtn>이용약관</TxtBtn>
           <Txt16>에 동의합니다</Txt16>
         </FormContentLeft>
         <FormContentLeft>
-          <FormCheckbox type="checkbox" />
+          <FormCheckbox type="checkbox" name="checkedPersonalInfo" value={formData.checkedPersonalInfo} onChange={handleInputChange} />
           <Txt16>(필수)</Txt16>
           <TxtBtn>개인정보 수집 이용</TxtBtn>
           <Txt16>에 동의합니다</Txt16>
         </FormContentLeft>
         </FormLayout>
-
-        <LoginCTA>
+        <LoginCTA onClick={onClickSignupBtn}>
           <Txt24Bold>회원가입</Txt24Bold>
         </LoginCTA>
       </FormContainer>
