1a763a8035bb9731f0fd5321e5cb0373a1185732
Sunbeom <wsbs9805@naver.com>
2024-11-13 11:01:15 +0900

    :memo: axios 관련 세팅 추가

--- src/services/client.js
+++ src/services/client.js
@@ -1,15 +1,19 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
+import https from "https";
 
-const withCredentials = true;
 
 // const URL = window.location.href;
-
 // const baseURL = "http://221.146.234.20:80";
+
+const withCredentials = true;
 const baseURL = "https://221.146.234.20";
 
 const client = axios.create({
-  baseURL,
-  withCredentials,
+  baseURL : baseURL,
+  withCredentials : withCredentials,
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false // 자체 서명 인증서 허용
+  })
 });
 
 // 요청 인터셉터를 추가하여 모든 요청에 대해 헤더를 설정합니다.
