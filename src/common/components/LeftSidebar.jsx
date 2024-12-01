40a44ce8eaba382d8fecae00fb5280473302ecc9
Sunbeom <wsbs9805@naver.com>
2024-12-01 15:49:37 +0900

    ✨ 로고 기능 추가
    - 로고 클릭 시, 홈으로 이동

--- src/common/components/LeftSidebar.jsx
+++ src/common/components/LeftSidebar.jsx
@@ -11,6 +11,8 @@ import {
   Txt16,
   Txt16Bold,
   LeftMenu,
+  AddButton,
+  FlexContainer,
 } from "../common";
 import DatePicker from "react-datepicker";
 import Icon_Home from "../../assets/Home.svg";
@@ -18,16 +20,24 @@ import Icon_Directory from "../../assets/Directory.svg";
 import Icon_VerticalMore from "../../assets/VerticalMore.svg";
 import Icon_Trash from "../../assets/Trash.svg";
 import "react-datepicker/dist/react-datepicker.css";
-import { useQuery } from "@tanstack/react-query";
-import { getFolders } from "../utils";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { createFolder, getFolders } from "../utils";
+import { useNavigate } from "react-router-dom";
 
 export const LeftSidebar = () => {
+  const navigate = useNavigate();
+
+  const handleNavigateToHome = () => {
+    navigate(`/`);
+  };
+
+
   const [selectedDate, setSelectedDate] = useState(null);
 
-  const [selectedCategory, setSelectedCategory] = useState("");
+  // const [selectedCategory, setSelectedCategory] = useState("");
 
-  const { data: categories } = useQuery({
-    queryKey: ["categories"],
+  const { data: folders } = useQuery({
+    queryKey: ["folders"],
     queryFn: async () => await getFolders({}),
     onSuccess: () => {
       console.log("success");
@@ -38,30 +48,49 @@ export const LeftSidebar = () => {
     refetchOnMount: "always",
   });
 
+  const onCreateFolder = useMutation({
+    mutationFn: createFolder,
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log("에러 발생! 아래 메시지를 확인해주세요.", error);
+    },
+  });
+
+  const onClickCreateFolder = async () => {
+    onCreateFolder.mutate("고구마깡은 감자도리");
+  };
+
   return (
     <LeftMenuContainer>
       <LeftMenu>
         {/* 추후에 Logo 컴포넌트로 분리 */}
-        <LogoTxt>BENTO</LogoTxt>
+        <LogoTxt onClick={handleNavigateToHome}>BENTO</LogoTxt>
         <InnerLeftMenu>
           <LeftMenuItem>
-            <CustomIcon src={Icon_Home} />
-            <Txt16Bold>홈</Txt16Bold>
+            <FlexContainer>
+              <CustomIcon src={Icon_Home} />
+              <Txt16Bold>홈</Txt16Bold>
+            </FlexContainer>
+            {/* <AddButton onClick={onClickCreateFolder}>+</AddButton> */}
           </LeftMenuItem>
           <DirectoryList>
-            {categories?.map((category, index) => (
+            {folders?.map((folder, index) => (
               <DirectoryContainer key={index}>
                 <DirectoryItem>
                   <CustomIcon src={Icon_Directory} />
-                  <Txt16>{category}</Txt16>
+                  <Txt16>{folder?.folderName}</Txt16>
                 </DirectoryItem>
                 <CustomIcon src={Icon_VerticalMore} />
               </DirectoryContainer>
             ))}
           </DirectoryList>
           <LeftMenuItem>
-            <CustomIcon src={Icon_Trash} />
-            <Txt16Bold>휴지통</Txt16Bold>
+            <FlexContainer>
+              <CustomIcon src={Icon_Trash} />
+              <Txt16Bold>휴지통</Txt16Bold>
+            </FlexContainer>
           </LeftMenuItem>
         </InnerLeftMenu>
 
