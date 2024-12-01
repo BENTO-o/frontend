e5e48e6127813b11328f21eec39ad370c0caf490
Sunbeom <wsbs9805@naver.com>
2024-12-01 15:45:13 +0900

    ✨ createNote 수정
    - 노트 생성 API 규격 수정에 따른 수정 사항 발생
    - 업로드 or 녹음 조건부 렌더링 추가
    - 제목 작성 가능하도록 수정

--- src/domains/createNote/components/CreateNoteForm.jsx
+++ src/domains/createNote/components/CreateNoteForm.jsx
@@ -5,7 +5,9 @@ import {
   FieldItem,
   FieldListContainer,
   FileUploadContainer,
+  FlexContainer,
   FormGroup,
+  FormTitleContainer,
   Input,
   Label,
   MemoItem,
@@ -15,6 +17,7 @@ import {
   NoteContainer,
   SubmitButton,
   Txt20Bold,
+  Txt24Bold,
 } from "../../../common/common";
 import dayjs from "dayjs";
 import { DirectorySelect } from "../../../common/components/DirectorySelect";
@@ -22,25 +25,58 @@ import { RecordBox } from "./RecordBox";
 import { RecordUpload } from "./RecordUpload";
 import { Memo } from "../../../common/components/Memo";
 import { NoteField } from "../../../common/components/NoteField";
+import { useCreateNoteFormStore } from "../../../stores/useCreateNoteForm";
+import { useMutation } from "@tanstack/react-query";
+import { createNote } from "../services";
 
-export const CreateNoteForm = () => {
+export const CreateNoteForm = (props) => {
+  const { form, setFormField, resetForm } = useCreateNoteFormStore(); // zustand 훅 사용하여 form 상태 가져오기
+
+  const onCreate = useMutation({
+    mutationFn: createNote,
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log("에러 발생! 아래 메시지를 확인해주세요.", error);
+    },
+  });
+
+  const onClickCreate = async () => {
+    onCreate.mutate(form);
+  };
+
+  const handleTitleChange = (e) => {
+    setFormField("title", e.target.value);
+  };
+
   return (
     <NoteContainer>
-      <Txt20Bold>2024년 10월 4일 00:00의 새로운 노트</Txt20Bold>
-      {/* <FormGroup>
-        <Label>참석자</Label>
-        <Input type="text" value="지연우" readOnly />
-      </FormGroup> */}
+      <Txt24Bold>노트 생성하기</Txt24Bold>
+      <FlexContainer justifyContent="space-between" width="100%">
+        <FlexContainer flexDirection="column" alignItems="flex-start">
+          <FormTitleContainer>
+            <Txt20Bold>제목</Txt20Bold>
+          </FormTitleContainer>
+          <Input
+            value={form.title}
+            onChange={handleTitleChange}
+            placeholder="제목을 입력해주세요"
+          />
+        </FlexContainer>
+        <AddButton onClick={onClickCreate}> 노트 생성 </AddButton>
+      </FlexContainer>
 
       <DirectorySelect />
 
       <NoteField />
 
-      <RecordUpload />
+      {!props.isRecord && (<RecordUpload />)}
 
-      <Memo />
+      {/* <Memo /> */}
 
-      <RecordBox />
+      
+      {props.isRecord && (<RecordBox />)}
     </NoteContainer>
   );
 };
