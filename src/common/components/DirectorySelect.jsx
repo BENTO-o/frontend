c784e9d226b552ee63c75cd321d0602bd8a4a66e
Sunbeom <wsbs9805@naver.com>
2024-12-01 15:43:16 +0900

    ✨ folder 기능 수정
    - zustand를 사용해 상태관리

--- src/common/components/DirectorySelect.jsx
+++ src/common/components/DirectorySelect.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   FlexContainer,
   FormGroup,
   FormTitleContainer,
-  Input,
+  Select,
+  SelectWrapper,
   Txt16Bold,
   Txt18Bold,
 } from "../common";
 import { useQuery } from "@tanstack/react-query";
 import { getFolders } from "../utils";
+import { useCreateNoteFormStore } from "../../stores/useCreateNoteForm";
 
 export const DirectorySelect = () => {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const { form, setFormField } = useCreateNoteFormStore(); // zustand 후복을 사용하여 form 상태 가져오기
 
-  const { data: categories } = useQuery({
-    queryKey: ["categories"],
+  const { data: folders } = useQuery({
+    queryKey: ["folders"],
     queryFn: async () => await getFolders({}),
     onSuccess: () => {
-      // console.log("success");
-      console.log(categories);
+      console.log(folders);
     },
     onError: (e) => {
       console.error(e);
@@ -26,10 +27,11 @@ export const DirectorySelect = () => {
     refetchOnMount: "always",
   });
 
-
-  // 사용자가 input에서 선택하거나 새롭게 입력하는 이벤트 처리
-  const handleCategoryChange = (e) => {
-    setSelectedCategory(e.target.value.trim());
+  // select 에서 선택한 것을 처리하는 이벤트
+  const handleFolderChange = (e) => {
+    const selectedValue = e.target.value;
+    setFormField("folder", selectedValue); // 폼의 folder 필드를 업데이트
+    console.log(selectedValue); // 업데이트된 값을 지정 출력
   };
 
   return (
@@ -38,22 +40,27 @@ export const DirectorySelect = () => {
         <Txt18Bold>카테고리 선택</Txt18Bold>
       </FormTitleContainer>
       <FlexContainer>
-        <Input
-          list="category-options"
-          value={selectedCategory}
-          onChange={handleCategoryChange}
-          placeholder="카테고리 선택 또는 새 카테고리 입력"
-        />
-        <datalist id="category-options">
-          {categories?.map((category, index) => (
-            <option key={index} value={category} />
-          ))}
-        </datalist>
-        {selectedCategory && (
+        <SelectWrapper>
+          <Select
+            value={form.folder}
+            onChange={handleFolderChange}
+            placeholder="카테고리 선택"
+          >
+            <option value="" disabled>
+              카테고리 선택
+            </option>
+            {folders?.map((folder, index) => (
+              <option key={index} value={folder?.folderName}>
+                {folder?.folderName}
+              </option>
+            ))}
+          </Select>
+        </SelectWrapper>
+        {/* {form.folder && (
           <div>
-            <Txt16Bold>선택된 카테고리: {selectedCategory}</Txt16Bold>
+            <Txt16Bold>선택된 카테고리: {form.folder}</Txt16Bold>
           </div>
-        )}
+        )} */}
       </FlexContainer>
     </FormGroup>
   );
