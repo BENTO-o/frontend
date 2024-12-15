174501fa86bdec6be07182c86ddb61888ba43844
Sunbeom <wsbs9805@naver.com>
2024-12-16 08:32:29 +0900

    ✨ 엔터키 로그인 / 회원가입

--- src/domains/signup/Signup.jsx
+++ src/domains/signup/Signup.jsx
@@ -19,7 +19,6 @@ import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 
 function Signup() {
-  // const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
@@ -30,12 +29,12 @@ function Signup() {
     checkedRule: false,
     checkedPersonalInfo: false,
   });
-  
+
   const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [name]: type === 'checkbox' ? checked : value
+      [name]: type === 'checkbox' ? checked : value,
     }));
   };
 
@@ -50,7 +49,8 @@ function Signup() {
     },
   });
 
-  const onClickSignupBtn = async () => {
+  const onClickSignupBtn = async (e) => {
+    e.preventDefault(); // 폼 제출 동작 방지
     if (formData.password !== formData.passwordConfirm) {
       alert('비밀번호가 일치하지 않습니다.');
       return;
@@ -65,49 +65,82 @@ function Signup() {
     }
   };
 
+  const handleNavigateToHome = () => {
+    navigate(`/`);
+  };
+
   return (
     <PageLayout>
-      <LogoTxt>BENTO</LogoTxt>
-      <FormContainer>
-        <Txt24Bold>회원가입</Txt24Bold>
-        <FormLayout mt="20px" mb="20px">
-          <FormContentVertical>
-            <Txt20>이메일</Txt20>
-            <LoginFormInput type="text" name="email" value={formData.email} onChange={handleInputChange} />
-          </FormContentVertical>
-          {/* <FormContentVertical>
-            <Txt20>이름</Txt20>
-            <LoginFormInput type="text" name="username" value={formData.username} onChange={handleInputChange} />
-          </FormContentVertical> */}
-          <FormContentVertical>
-            <Txt20>닉네임</Txt20>
-            <LoginFormInput type="text" name="username" value={formData.username} onChange={handleInputChange} />
-          </FormContentVertical>
-          <FormContentVertical>
-            <Txt20>비밀번호</Txt20>
-            <LoginFormInput type="password" name="password" value={formData.password} onChange={handleInputChange} />
-          </FormContentVertical>
-          <FormContentVertical>
-            <Txt20>비밀번호 확인</Txt20>
-            <LoginFormInput type="password" name="passwordConfirm" value={formData.passwordConfirm} onChange={handleInputChange} />
-          </FormContentVertical>
-          <FormContentLeft>
-          <FormCheckbox type="checkbox" name="checkedRule" value={formData.checkedRule} onChange={handleInputChange} />
-          <Txt16>(필수)</Txt16>
-          <TxtBtn>이용약관</TxtBtn>
-          <Txt16>에 동의합니다</Txt16>
-        </FormContentLeft>
-        <FormContentLeft>
-          <FormCheckbox type="checkbox" name="checkedPersonalInfo" value={formData.checkedPersonalInfo} onChange={handleInputChange} />
-          <Txt16>(필수)</Txt16>
-          <TxtBtn>개인정보 수집 이용</TxtBtn>
-          <Txt16>에 동의합니다</Txt16>
-        </FormContentLeft>
-        </FormLayout>
-        <LoginCTA onClick={onClickSignupBtn}>
+      <LogoTxt onClick={handleNavigateToHome}>BENTO</LogoTxt>
+      {/* form 태그로 수정 */}
+      <form onSubmit={onClickSignupBtn}>
+        <FormContainer>
           <Txt24Bold>회원가입</Txt24Bold>
-        </LoginCTA>
-      </FormContainer>
+          <FormLayout mt="20px" mb="20px">
+            <FormContentVertical>
+              <Txt20>이메일</Txt20>
+              <LoginFormInput
+                type="text"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+            </FormContentVertical>
+            <FormContentVertical>
+              <Txt20>닉네임</Txt20>
+              <LoginFormInput
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleInputChange}
+              />
+            </FormContentVertical>
+            <FormContentVertical>
+              <Txt20>비밀번호</Txt20>
+              <LoginFormInput
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+            </FormContentVertical>
+            <FormContentVertical>
+              <Txt20>비밀번호 확인</Txt20>
+              <LoginFormInput
+                type="password"
+                name="passwordConfirm"
+                value={formData.passwordConfirm}
+                onChange={handleInputChange}
+              />
+            </FormContentVertical>
+            <FormContentLeft>
+              <FormCheckbox
+                type="checkbox"
+                name="checkedRule"
+                checked={formData.checkedRule}
+                onChange={handleInputChange}
+              />
+              <Txt16>(필수)</Txt16>
+              <TxtBtn>이용약관</TxtBtn>
+              <Txt16>에 동의합니다</Txt16>
+            </FormContentLeft>
+            <FormContentLeft>
+              <FormCheckbox
+                type="checkbox"
+                name="checkedPersonalInfo"
+                checked={formData.checkedPersonalInfo}
+                onChange={handleInputChange}
+              />
+              <Txt16>(필수)</Txt16>
+              <TxtBtn>개인정보 수집 이용</TxtBtn>
+              <Txt16>에 동의합니다</Txt16>
+            </FormContentLeft>
+          </FormLayout>
+          <LoginCTA type="submit"> {/* 버튼 타입을 submit으로 설정 */}
+            <Txt24Bold>회원가입</Txt24Bold>
+          </LoginCTA>
+        </FormContainer>
+      </form>
     </PageLayout>
   );
 }
